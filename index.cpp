b884b56e6b34b47ef0390bc943acff992d1d902e
ankan <ankanmajumdar812@gmail.com>
2026-04-04 09:59:01 +0530

    to add new cpp work

---
+++ index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int peakIndex(int arr[], int n) {
+  int s = 0, e = n - 1;
+  int mid = s + (e - s) / 2;
+  while (s < e) {
+    if (arr[mid] < arr[mid + 1]) {
+      s = mid + 1;
+    } else {
+      e = mid;
+    }
+    mid = s + (e - s) / 2;
+  }
+  return s;
+}
+int main() {
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 6, 5, 4};
+  int peak = peakIndex(arr, 10);
+  cout << "peak index is :" << peak << endl;
+}
